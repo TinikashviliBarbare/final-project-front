507f07ebd61d7ea04f4937176472b3308f981ec5
BarbareTinikashvili <elgaareshidze1@gmail.com>
2026-05-14 19:16:47 +0400

    img slyde

--- frontend/src/layout/Main.jsx
+++ frontend/src/layout/Main.jsx
@@ -1,41 +1,89 @@
 import React, { useEffect, useState } from "react";
 import "../css/style.scss";
-import img from "../assets/firstphone.png";
+import img1 from "../assets/firstphone.png";
+import img2 from "../assets/phone2x.png";
+import img3 from "../assets/phone3.webp";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-
 
 function Main() {
+  const slides = [
+    {
+      text:"NOW IN OBSIDIAN",
+      image: img1,
+      price: "From $899",
+      baseText: "Time for the",
+      button: "BUY",
+      style: "slide1",
+      layout: "default",
+    },
+    {
+      image: img2,
+      price: "All new Prime Solar S24 Plus",
+      baseText: "Sharp shots,",
+      coloredText: "every",
+      secondBaseText: "time",
+      button: "PRE-ORDER",
+      style: "slide2",
+      layout: "default",
+    },
+    {
+      image: img3,
+      price: "From $1099",
+      baseText: "Experience ",
+      secondBaseText1: " ultimate",
+      button: "HSOP NOW",
+      style: "slide3",
+      layout: "rightText",
+    },
+  ];
+
+  const [slideIndex, setSlideIndex] = useState(0);
   const [text, setText] = useState("next level?");
-  const [key, setKey] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setText((prev) =>
-        prev === "next level?" ? "upgrade?" : "next level?"
-      );
+      setSlideIndex((prev) => (prev + 1) % slides.length);
+    }, 5000);
 
-      setKey((prev) => prev + 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText((prev) => (prev === "next level?" ? "upgrade?" : "next level?"));
     }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <div className="slide-show">
-      <img className="first-img" src={img} alt="" />
-      <div className="img-info">
-        <p className="text">NOW IN OBSIDIAN</p>
-        <h2>Time for the</h2>
+  const current = slides[slideIndex];
 
-        <h2 key={key} className="text-change">
-          {text}
+  return (
+    <div className={`slide-show ${current.style}`}>
+      <img className="first-img" src={current.image} alt="" />
+
+      <div
+        className={`img-info ${
+          current.layout === "rightText" ? "right" : ""
+        }`}>
+        <p className="text">{current.text}</p>
+
+        <h2>{current.baseText}</h2>
+        <h2> <span className="colored-text">{current.coloredText}</span>{current.secondBaseText}</h2>
+        <h2>{current.secondBaseText1}</h2>
+
+        <h2 className="text-change">
+          {slideIndex === 0 ? text : " "}
         </h2>
 
-        <p className="price">From $899</p>
-        <button className="buy"><b>BUY </b> <FontAwesomeIcon className="button-arrow" icon={faArrowRight} /></button>
-        
+        <p className="price">{current.price}</p>
+
+        <button className="buy">
+          <p>{current.button}</p>
+          <FontAwesomeIcon className="button-arrow" icon={faArrowRight} />
+        </button>
       </div>
     </div>
   );
